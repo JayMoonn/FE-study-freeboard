e6bff7c77d9c640172e2f689a7d4af788b7f23d1
jaymoonn <sjc01214@naver.com>
2022-12-31 17:19:43 +0900

    프론트엔드 강의 1회차

---
+++ class/pages/01-01-qqq/index.js
@@ -0,0 +1,10 @@
+export default function Qqq() {
+
+    return (
+        <>
+            <div>hello</div>
+            <button>click!!</button>
+        </>
+    )
+
+}
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,14 @@
+import { Email, EmailInput } from "../../styles/emotion"
+
+export default function EmotionPage() {
+
+    return (
+        <div>
+            <Email>이메일</Email>
+            <EmailInput type="text" />
+            <button>click!!</button>
+            <img src="/vercel.svg" />
+        </div>
+    )
+
+}
